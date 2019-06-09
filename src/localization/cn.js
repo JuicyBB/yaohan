45e8ad0803d52c7c78f8cad2a402bd1112ec0f3b
Leslie Chan <leslie.chan@live.hk>
2019-06-09 13:53:31 -0700

    .gitignore Fixed

---
+++ src/localization/cn.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'locale': 'cn',
+
+    // Nav
+
+    'Shopping Mall Hours' : 'Shopping Mall Hours CN',
+    // Map Section
+    'Monday to Sunday' : 'Monday to Sunday CN'
+}
---
+++ src/localization/en.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'locale': 'en',
+
+    // Nav
+
+    'Shopping Mall Hours' : 'Shopping Mall Hours ENS',
+    // Map Section
+    'Monday to Sunday' : 'Monday to Sunday ENS'
+}
