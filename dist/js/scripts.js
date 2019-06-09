6a33e742e3aaf35cb1464e13f4cb97441d288229
JuicyBB <joyce.leung@live.hk>
2019-06-09 13:42:34 -0700

    Add blog page with read more

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -23,3 +23,39 @@
     }
   }));
 })();
+
+/**
+ * =======================
+ * Read More
+ * =======================
+ */
+(function() {
+  $(document).ready((function() {
+    var maxHeight = 150;
+
+    $("#news_blog .card").each((function() {
+      var current = $(this).find(".card-text");
+      // truncate if more than max height
+      if (current.height() > maxHeight) {
+        current.addClass("truncated-text");
+      } else {
+        // remove more link if less than max height
+        current
+          .parents(".card")
+          .find(".more-link")
+          .remove();
+      }
+    }));
+
+    var untruncate = function() {
+      var card = $(this)
+        .parents(".card")
+        .find(".card-text.truncated-text");
+      card.removeClass("truncated-text");
+      $(this).unbind("click", untruncate);
+      $(this).remove();
+    };
+
+    $(".more-link").bind("click", untruncate);
+  }));
+})();
