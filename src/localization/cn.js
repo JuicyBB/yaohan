dee4e21a8f6cd72656585db1b6bdfe2d17f6d215
Leslie Chan <leslie.chan@live.hk>
2019-06-23 00:53:27 -0700

    YAOC-7 Add translation placeholders for all pages

--- src/localization/cn.js
+++ src/localization/cn.js
@@ -15,12 +15,25 @@ module.exports = {
 
     // Index
     'Shopping Mall Hours' : '商場營業時間',
+    'Home_Intro_1' : '八佰伴中心 是加拿大卑詩省列治文市第一批發展成功之華人商場,於一九九三年開業，至現時為止八佰伴中心仍然是本地居民及旅客的購物地標。',
+    'Home_Intro_2' : '八佰伴中心為顧客提供完全的一站式購物設備，無需東奔西走，是您最佳的選擇！顧客可在寬敞空間的舒適室內環境中，盡情選購各式精品。八佰伴中心內貨品及服務包羅萬有，包括名牌時裝、手飾精品、東方工藝、花店、畫廊、健康食品用品、珠寶首飾、名牌化妝品、家庭電器及家居用品、錢幣找換、旅行社、各類保險、醫務所、地產公司、髮廊音樂唱片及中日書店。而著名的超級市場更提供不同種類日色及中色產品.',
+    'Latest News & Events' : 'Latest News & Events1',
+    'See all news & events' : 'See all news & events1',
+    'View Article' : 'View Article1',
+    'Eat, shop and more.' : 'Eat, shop and more.1',
 
     // Mall Hours
     'Monday to Sunday' : '星期一至星期日',
     '1st Floor' : '商場一樓',
     '2nd Floor' : '商場二樓',
     'Supermarket' : '超級市場',
+    'Address & Map' : 'Address & Map1',
+
+    // Directions
+    'Visit Yaohan Centre' : 'Visit Yaohan Centre1',
+    'Address, map and phone' : 'Address, map and phone1',
+    'Google maps direction' : 'Google maps direction1',
+    'Send us a message' : 'Send us a message1',
 
     // Directory
     'Food & Grocery' : '食品雑貨',
@@ -29,4 +42,26 @@ module.exports = {
     'Arts & Craft' : '手工藝品',
     'Gifts & Hobby' : '禮物休閑',
     'Specialty & Service' : '其它服務',
+
+    // News
+    "Find out what's happening at Yaohan Centre" : "Find out what's happening at Yaohan Centre1",
+    'Read more' : 'Read more1',
+    'Load more posts' : 'Load more posts1',
+
+    //Contact
+    'Close' : '1Close',
+    'Your message has been sent!' : '1Your message has been sent!',
+    'Something went wrong, please try again.' : '1Something went wrong, please try again.',
+    'Find us' : '1Find us',
+    'Visit Yaohan Centre in Richmond' : '1Visit Yaohan Centre in Richmond',
+    'All fields are required.' : '1All fields are required.',
+    'Full Name' : '1Full Name',
+    'Email Address' : '1Email Address',
+    'Phone Number' : '1Phone Number',
+    'Message' : '1Message',
+    'Send Message' : '1Send Message',
+    'Enter full name' : '1Enter full name',
+    'Enter email' : '1Enter email',
+    'Enter phone number' : '1Enter phone number',
+    'Enter your message' : '1Enter your message',
 };
--- src/localization/en.js
+++ src/localization/en.js
@@ -15,12 +15,26 @@ module.exports = {
 
     // Index
     'Shopping Mall Hours' : 'Shopping Mall Hours',
+    'Home_Intro_1' : 'Yaohan Centre is one of the earliest Asian Malls developed in Richmond, B.C. Yaohan Centre still shares the popularity of Richmond, B.C. for retail destination while maintaining its uniqueness in up class shopping.',
+    'Home_Intro_2' : 'Much more than a Mall, Yaohan Centre is a place where you can have lots of fun with friends and family. We offer a wealth of products and services including fine fashions, souvenirs, gifts, oriental arts &amp; crafts, paintings, herbal health food, toys, jewelry, cosmetics, home appliances, realty, floral, insurance, foreign exchange, travel and medical needs at your convenience.',
+    'Home_Intro_3' : 'For relaxation and enjoyment, visit the salons, music and book stores. You can also visit our famous supermarket which provides a wide range of Japanese and Chinese Products.',
+    'Latest News & Events' : 'Latest News & Events',
+    'See all news & events ' : 'See all news & events ',
+    'View Article' : 'View Article',
+    'Eat, shop and more.' : 'Eat, shop and more.',
 
     // Mall Hours
     'Monday to Sunday' : 'Monday to Sunday',
     '1st Floor' : '1st Floor',
     '2nd Floor' : '2nd Floor',
     'Supermarket' : 'Supermarket',
+    'Address & Map' : 'Address & Map',
+
+    // Directions
+    'Visit Yaohan Centre' : 'Visit Yaohan Centre',
+    'Address, map and phone' : 'Address, map and phone',
+    'Google maps direction' : 'Google maps direction',
+    'Send us a message' : 'Send us a message',
 
     // Directory
     'Food & Grocery' : 'Food & Grocery',
@@ -29,4 +43,26 @@ module.exports = {
     'Arts & Craft' : 'Arts & Craft',
     'Gifts & Hobby' : 'Gifts & Hobby',
     'Specialty & Service' : 'Specialty & Service',
+
+    // News
+    "Find out what's happening at Yaohan Centre" : "Find out what's happening at Yaohan Centre1",
+    'Read more' : 'Read more',
+    'Load more posts' : 'Load more posts',
+
+    //Contact
+    'Close' : 'Close',
+    'Your message has been sent!' : 'Your message has been sent!',
+    'Something went wrong, please try again.' : 'Something went wrong, please try again.',
+    'Find us' : 'Find us',
+    'Visit Yaohan Centre in Richmond' : 'Visit Yaohan Centre in Richmond',
+    'All fields are required.' : 'All fields are required.',
+    'Full Name' : 'Full Name',
+    'Email Address' : 'Email Address',
+    'Phone Number' : 'Phone Number',
+    'Message' : 'Message',
+    'Send Message' : 'Send Message',
+    'Enter full name' : 'Enter full name',
+    'Enter email' : 'Enter email',
+    'Enter phone number' : 'Enter phone number',
+    'Enter your message' : 'Enter your message',
 };
