1eed84ea6f39a17caf220401fe7b16093cb6499c
Leslie Chan <lesliec@ensemble.com>
2019-06-25 16:21:00 -0700

    Update contact form to use epikode API

--- src/js/pages/contact.js
+++ src/js/pages/contact.js
@@ -1,10 +1,3 @@
-$(function() {
-  document.getElementById('contactFrame').addEventListener('load', function() { 
-    // Contact form success
-    console.log('frame load');
-  });
-});
-
 /**
  * =======================
  * Contact Form
@@ -15,5 +8,25 @@ $(function() {
     form: "#contactForm",
     errorMessageClass: "invalid-feedback",
     errorElementClass: "invalid",
+  });
+
+  $("#contactForm").submit(function(e) {
+    $('.alert').addClass('d-none');
+
+    e.preventDefault();
+    $.ajax({
+      url: "https://epk-api.herokuapp.com/api/yaohan/email",
+      method: "POST",
+      data: $(this).serialize(),
+      success: function() {
+        $('.alert-success').removeClass('d-none');
+        $('#contactForm').addClass('d-none');
+        $("html, body").animate({ scrollTop: "0px" });
+      },
+      error: function() {
+        $('.alert-danger').removeClass('d-none');
+        $("html, body").animate({ scrollTop: "0px" });
+      }
+    });
   });
 });
